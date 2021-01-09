dc29836d7eed252f54e9a5eece133a9ff5faae35
Rami El Zohbi <rami@eaglevisionqatar.com>
2021-01-09 14:43:26 +0300

    Add static page uploads

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use(cors({
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use('/uploads', express.static('uploads'));
 app.use(express.urlencoded({ extended: false }));
 
 app.use(cookieParser());
